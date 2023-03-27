1a1c2352533c9c37863597102571e5c6050a2861
Priya Arora <arorapiya141@gmail.com>
2023-03-27 15:53:04 +0530

    Testing stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import DetailState from './context/detail/DetailState'
-import AuthState from './context/auth/AuthState';
-import Register from './components/authentication/Register';
-import Login from './components/authentication/Login';
-import AlertState from './context/alert/AlertState';
-import Alerts from './components/layout/Alerts';
-import setAuthToken from './utils/setAuthToken'
-import PrivateRoute from './components/routing/PrivateRoute';
 import FormCont from './components/authentication/FormCont';
+import Alerts from './components/layout/Alerts';
+import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import PrivateRoute from './components/routing/PrivateRoute';
+import AlertState from './context/alert/AlertState';
+import AuthState from './context/auth/AuthState';
+import DetailState from './context/detail/DetailState';
+import setAuthToken from './utils/setAuthToken';
 if (localStorage.token) {
   setAuthToken(localStorage.token)
 }
@@ -32,7 +30,6 @@ const App = () => {
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={FormCont} />
                   <Route exact path='/login' component={FormCont} />
-
                 </Switch>
               </div>
             </Fragment>
